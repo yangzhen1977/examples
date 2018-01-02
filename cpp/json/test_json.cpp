22caf825ddaf1415626054d31468904eae726bba
Zhen Yang <yangzhen1977@outlook.com>
2018-01-02 17:55:21 +0800

    added json

---
+++ cpp/json/test_json.cpp
@@ -0,0 +1,16 @@
+// Example program
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <regex>
+
+#include <json/json.h>
+
+
+int main(int argc, char *argv[])
+{
+    Json::Value string_;
+    std::cout <<"Hello World" << std::endl;
+    return 0;
+}
