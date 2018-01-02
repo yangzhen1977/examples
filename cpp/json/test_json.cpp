10079dd507ec30954438f64e75815f1b5b52afd4
Zhen Yang <yangzhen1977@outlook.com>
2018-01-02 18:30:19 +0800

    added example for json

--- cpp/json/test_json.cpp
+++ cpp/json/test_json.cpp
@@ -2,15 +2,55 @@
 #include <cassert>
 #include <cstdlib>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <regex>
 
 #include <json/json.h>
 
+//https://open-source-parsers.github.io/jsoncpp-docs/doxygen/index.html
 
+int test_json1(int argc, char* argv[])
+{
+    Json::CharReaderBuilder rbuilder;
+    rbuilder["collectComments"] = false;
+
+    std::string errs;
+    std::string str = "{\"1\":\"2\"}";
+    std::stringstream ss;
+    ss << str;
+    ss.flush();
+
+    Json::Value root;
+    bool ok = Json::parseFromStream(rbuilder, ss, &root, &errs);
+    if(!ok){
+        std::cerr << "test_json1 failed, errs is " << errs << std::endl;
+    }
+    return 0;
+}
+
+int test_json2(int argc, char *argv[])
+{
+    Json::CharReaderBuilder rbuilder;
+    rbuilder["collectComments"] = false;
+
+    std::string str = "{\"1\":\"2\"}";
+    Json::Value root;
+
+    Json::CharReaderBuilder builder;
+    Json::CharReader *reader = builder.newCharReader();
+    std::string errs;
+    bool ok = reader->parse(str.c_str(), str.c_str() + str.length() +1, &root, &errs);
+    if(!ok){
+        std::cerr << errs << std::endl;
+    }
+    assert(ok);
+    delete(reader);
+    return 0;
+}
 int main(int argc, char *argv[])
 {
-    Json::Value string_;
-    std::cout <<"Hello World" << std::endl;
+    test_json1(argc, argv);
+    test_json2(argc, argv);
     return 0;
 }
