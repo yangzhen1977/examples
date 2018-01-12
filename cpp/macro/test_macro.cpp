760c3084114d9f3e3be2dbdb2b5ea5014b848bf0
Zhen Yang <yangzhen1977@outlook.com>
2018-01-12 16:18:32 +0800

    test macro string as variable part

--- cpp/macro/test_macro.cpp
+++ cpp/macro/test_macro.cpp
@@ -21,9 +21,9 @@ std::tuple<int32_t, std::string> test_1( std::string index)
         res = test_##index(#index);   \
     }while(0)
 
-int main(int argc, char *argv[])
+void test1()
 {
-    assert( -1 == g_value_1);
+    assert(-1 == g_value_1);
     std::tuple<int32_t, std::string> res;
     FUNC(1);
     int32_t res1 = std::get<0>(res);
@@ -33,5 +33,33 @@ int main(int argc, char *argv[])
     assert(1 == res1);
     assert(std::string("1") == res2);
     assert(1 == g_value_1);
+}
+struct CS
+{
+    std::string attr_ = "attr default";
+    std::string is_attr_ = "is_attr default";
+};
+CS g_cs;
+#define FUNC2(key)                    \
+    do                                \
+    {                                 \
+        g_cs.key##_ = "attr changed"; \
+        g_cs.is_##key##_ = "is attr changed"; \
+    } while (0)
+
+void test2()
+{
+    std::cout << "attr_ is " << g_cs.attr_ << std::endl;
+    std::cout << "is_attr_ is " << g_cs.is_attr_ << std::endl;
+    FUNC2(attr);
+    std::cout << "attr_ is " << g_cs.attr_ << std::endl;
+    std::cout << "is_attr_ is " << g_cs.is_attr_ << std::endl;
+    assert(g_cs.attr_ == "attr changed");
+    assert(g_cs.is_attr_ == "is attr changed");
+}
+int main(int argc, char *argv[])
+{
+    test1();
+    test2();
     return 0;
 }
